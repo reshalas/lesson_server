0b9300c7bb48edef4a3624d4809c8c18a099d01c
risead <rishatfazlutdinov@gmail.com>
2023-08-24 21:33:20 +0500

    добавил красивое писмецо

--- src/utils.rs
+++ src/utils.rs
@@ -37,13 +37,42 @@ pub fn build_register_mesage(name: &str, verefication_key: &str, username: &str)
     let cancel_ref = format!("http://127.0.0.1:8090/verefication/email/cancel/{}/{}", username, verefication_key);
     format!("
     <div>
-        <p>Здравствуй, {}! C твоего аддреса электронной почты кто-то хочет зарегестрироваться в нашу маленькую и ламповою систему. Если это ты, то добро пожаловать. Смело нажимай на кнопку \"Законьчить регистрацию\", в противном случае прошу нажать на кнопку \"Отклонить регистрацию\"</p>
+        <p>Здаров, {}! C твоего аддреса электронной почты кто-то хочет зарегестрироваться в
+        систему Reshala Если это ты, то добро пожаловать. 
+         Смело нажимай на кнопку \"Подтвердить\", в противном случае прошу нажать 
+         на кнопку \"Отклонить\"</p>
 
             <a href=\"{}\"
-      style=\"background-color:#28b422;border-radius:4px;color:#ffffff;display:inline-block;font-family:sans-serif;font-size:13px;font-weight:bold;line-height:35px;text-align:center;text-decoration:none;-webkit-text-size-adjust:none;\">Законьчить регистрацию</a>
-      <a href=\"{}\"
-      style=\"background-color:#28b422;border-radius:4px;color:#ffffff;display:inline-block;font-family:sans-serif;font-size:13px;font-weight:bold;line-height:35px;text-align:center;text-decoration:none;width:120px;-webkit-text-size-adjust:none;\">Отклонить регистрацию</a>
+      style=\"background-color:#28b422;
+      border-radius:4px;
+      color:#ffffff;
+      display:inline-block;
+      font-family:sans-serif;
+      font-size:14px;
+      font-weight:bold;
+      line-height:35px;
+      text-align:center;
+      text-decoration:none;
+      -webkit-text-size-adjust:none;
+      \">Подтвердить</a>
+      
+            <a href=\"{}\"
+      style=\"background-color:#28b422;
+      border-radius:4px;
+      color:#ffffff;
+      display:inline-block;
+      font-family:sans-serif;
+      font-size:14px;
+      font-weight:bold;
+      line-height:35px;
+      text-align:center;
+      text-decoration:none;
+      width:120px;
+      -webkit-text-size-adjust:none;
+      \">Отклонить</a>
 
     </div>"
  , name, verfy_ref, cancel_ref)
 }
+
+
